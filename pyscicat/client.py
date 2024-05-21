e3e4d3467e571ea0517936ec3e65a76890683eb8
Morgan Wall <29980928+mkywall@users.noreply.github.com>
2024-05-21 13:46:36 -0700

    this bug has needed to be fixed for a long time

--- pyscicat/client.py
+++ pyscicat/client.py
@@ -111,7 +111,7 @@ class ScicatClient:
         response = self._send_to_scicat(cmd=cmd, endpoint=endpoint, data=data)
         result = response.json() if len(response.content) > 0 else None
         if not response.ok:
-            raise ScicatCommError(f"Error in operation {operation}: {result}")
+            raise ScicatCommError(f"Error in operation {operation}: {response}")
 
         logger.info(
             "Operation '%s' successful%s",
