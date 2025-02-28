b2ddce1910a8cbf761ab9739176fb0d6370d5513
katerinavr <katerina_vriza@yahoo.gr>
2025-02-27 18:06:09 -0600

    initial commit

--- examples/ingest_dataset.py
+++ examples/ingest_dataset.py
@@ -2,16 +2,18 @@ from datetime import datetime
 from pathlib import Path
 
 from pyscicat.client import encode_thumbnail, ScicatClient
-from pyscicat.model import Attachment, Datablock, DataFile, Dataset, Ownable
+from pyscicat.model import Attachment, Datablock, DataFile, Dataset, Ownable, OrigDatablock
 
 # Create a client object. The account used should have the ingestor role in SciCat
 scicat = ScicatClient(
-    base_url="http://localhost:3000/api/v3", username="Zaphod", password="heartofgold"
+    base_url="http://localhost:3000/api/v3", username="ingestor", password="aman"
 )
+print('scicat', scicat)
 
 # Create an Ownable that will get reused for several other Model objects
 ownable = Ownable(ownerGroup="magrathea", accessGroups=["deep_though"])
-thumb_path = Path(__file__).parent.parent / "test/data/SciCatLogo.png"
+# thumb_path = Path(__file__).parent.parent / "test/data/SciCatLogo.png"
+thumb_path = Path(r'C:\Users\avriza\Desktop\pyscicat\tests\test_pyscicat\data\SciCatLogo.png')
 
 
 # Create a RawDataset object with settings for your choosing. Notice how
@@ -33,14 +35,16 @@ dataset = Dataset(
     sampleId="gargleblaster",
     **ownable.dict()
 )
-dataset_id = scicat.upload_raw_dataset(dataset)
+dataset_id = scicat.upload_new_dataset(dataset)
+print('dataset_id', dataset_id)
 
 # Create Datablock with DataFiles
 data_file = DataFile(path="file.h5", size=42)
-data_block = Datablock(
-    size=42, version=1, datasetId=dataset_id, dataFileList=[data_file], **ownable.dict()
+data_block = OrigDatablock(
+    size=42, version='1', datasetId=dataset_id, dataFileList=[data_file], **ownable.dict()
 )
-scicat.upload_datablock(data_block)
+print(data_block.__dict__)
+scicat.upload_dataset_origdatablock(dataset_id=dataset_id, datablockDto=data_block)
 
 # Create Attachment
 attachment = Attachment(
